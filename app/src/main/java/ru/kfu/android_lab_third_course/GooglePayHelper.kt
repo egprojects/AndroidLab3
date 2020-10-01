62581c2b70b52f91001ff56424482f60e5601163
ergat <ru.egprojects@gmail.com>
2020-10-01 18:21:08 +0300

    Bugfix

--- app/src/main/java/ru/kfu/android_lab_third_course/GooglePayHelper.kt
+++ app/src/main/java/ru/kfu/android_lab_third_course/GooglePayHelper.kt
@@ -1,22 +1,17 @@
 package ru.kfu.android_lab_third_course
 
 import android.app.Activity
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.wallet.*
+import com.google.android.gms.wallet.PaymentsClient
+import com.google.android.gms.wallet.Wallet
+import com.google.android.gms.wallet.Wallet.WalletOptions
+import com.google.android.gms.wallet.WalletConstants
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class GooglePayHelper {
     companion object {
-        private val supportedPaymentMethods = listOf(
-            WalletConstants.PAYMENT_METHOD_CARD,
-            WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD
-        )
-        private val allowedCardNetworks = listOf(
-            WalletConstants.CARD_NETWORK_AMEX,
-            WalletConstants.CARD_NETWORK_JCB,
-            WalletConstants.CARD_NETWORK_DISCOVER,
-            WalletConstants.CARD_NETWORK_MASTERCARD,
-            WalletConstants.CARD_NETWORK_VISA
-        )
         private val environment: Int
             get() = if (BuildConfig.DEBUG) {
                 WalletConstants.ENVIRONMENT_TEST
@@ -24,50 +19,113 @@ class GooglePayHelper {
                 WalletConstants.ENVIRONMENT_PRODUCTION
             }
 
-        fun createPaymentsClient(activity: Activity): PaymentsClient {
-            val walletOptions = Wallet.WalletOptions.Builder()
-                .setEnvironment(environment)
-                .build()
-
-            return Wallet.getPaymentsClient(activity, walletOptions)
+        private fun getAllowedCardNetworks(): JSONArray {
+            return JSONArray()
+                .put("AMEX")
+                .put("DISCOVER")
+                .put("INTERAC")
+                .put("JCB")
+                .put("MASTERCARD")
+                .put("VISA")
         }
 
-        fun isReadyToPay(client: PaymentsClient): Task<Boolean> {
-            val request = IsReadyToPayRequest.newBuilder()
-                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
-                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
-                .build()
-
-            return client.isReadyToPay(request)
+        @Throws(JSONException::class)
+        private fun getBaseRequest(): JSONObject? {
+            return JSONObject().put("apiVersion", 2).put("apiVersionMinor", 0)
         }
 
-        fun createPaymentDataRequest(price: String): PaymentDataRequest {
-            val request = PaymentDataRequest.newBuilder()
-                .setTransactionInfo(getTransactionInfo(price))
-                .addAllowedPaymentMethods(supportedPaymentMethods)
-                .setCardRequirements(
-                    CardRequirements.newBuilder()
-                        .addAllowedCardNetworks(allowedCardNetworks)
-                        .build()
+        @Throws(JSONException::class)
+        private fun getGatewayTokenizationSpecification(): JSONObject? {
+            return object : JSONObject() {
+                init {
+                    put("type", "PAYMENT_GATEWAY")
+                    put("parameters", object : JSONObject() {
+                        init {
+                            put("gateway", "example")
+                            put("gatewayMerchantId", "exampleGatewayMerchantId")
+                        }
+                    })
+                }
+            }
+        }
+
+        private fun getAllowedCardAuthMethods(): JSONArray? {
+            return JSONArray()
+                .put("PAN_ONLY")
+                .put("CRYPTOGRAM_3DS")
+        }
+
+        @Throws(JSONException::class)
+        private fun getBaseCardPaymentMethod(): JSONObject? {
+            val cardPaymentMethod = JSONObject()
+            cardPaymentMethod.put("type", "CARD")
+            val parameters = JSONObject()
+            parameters.put("allowedAuthMethods", getAllowedCardAuthMethods())
+            parameters.put("allowedCardNetworks", getAllowedCardNetworks())
+            // Optionally, you can add billing address/phone number associated with a CARD payment method.
+            parameters.put("billingAddressRequired", true)
+            val billingAddressParameters = JSONObject()
+            billingAddressParameters.put("format", "FULL")
+            parameters.put("billingAddressParameters", billingAddressParameters)
+            cardPaymentMethod.put("parameters", parameters)
+            return cardPaymentMethod
+        }
+
+        @Throws(JSONException::class)
+        private fun getCardPaymentMethod(): JSONObject? {
+            val cardPaymentMethod = getBaseCardPaymentMethod()
+            cardPaymentMethod!!.put(
+                "tokenizationSpecification",
+                getGatewayTokenizationSpecification()
+            )
+            return cardPaymentMethod
+        }
+
+        fun createPaymentsClient(activity: Activity?): PaymentsClient? {
+            val walletOptions =
+                WalletOptions.Builder().setEnvironment(environment).build()
+            return Wallet.getPaymentsClient(activity!!, walletOptions)
+        }
+
+        fun getIsReadyToPayRequest(): JSONObject? {
+            return try {
+                val isReadyToPayRequest = getBaseRequest()
+                isReadyToPayRequest!!.put(
+                    "allowedPaymentMethods", JSONArray().put(getBaseCardPaymentMethod())
                 )
-            request.setPaymentMethodTokenizationParameters(createTokenizationParameters())
-
-            return request.build()
+                isReadyToPayRequest
+            } catch (e: JSONException) {
+                null
+            }
         }
 
-        private fun getTransactionInfo(price: String) =
-            TransactionInfo.newBuilder()
-                .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
-                .setTotalPrice(price)
-                .setCurrencyCode("USD")
-                .build()
+        @Throws(JSONException::class)
+        private fun getTransactionInfo(price: String): JSONObject? {
+            val transactionInfo = JSONObject()
+            transactionInfo.put("totalPrice", price)
+            transactionInfo.put("totalPriceStatus", "FINAL")
+            transactionInfo.put("countryCode", "RU")
+            transactionInfo.put("currencyCode", "USD")
+            return transactionInfo
+        }
 
-        private fun createTokenizationParameters(): PaymentMethodTokenizationParameters =
-            PaymentMethodTokenizationParameters.newBuilder()
-                .setPaymentMethodTokenizationType(WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
-                .addParameter("gateway", "stripe")
-                .addParameter("stripe:publishableKey", "Smth")
-                .build()
+        @Throws(JSONException::class)
+        private fun getMerchantInfo(): JSONObject? {
+            return JSONObject().put("merchantName", "Example Merchant")
+        }
 
+        fun getPaymentDataRequest(price: String): JSONObject? {
+            return try {
+                val paymentDataRequest: JSONObject = getBaseRequest()!!
+                paymentDataRequest.put(
+                    "allowedPaymentMethods", JSONArray().put(getCardPaymentMethod())
+                )
+                paymentDataRequest.put("transactionInfo", getTransactionInfo(price))
+                paymentDataRequest.put("merchantInfo", getMerchantInfo())
+                paymentDataRequest
+            } catch (e: JSONException) {
+                null
+            }
+        }
     }
 }
--- app/src/main/java/ru/kfu/android_lab_third_course/MainActivity.kt
+++ app/src/main/java/ru/kfu/android_lab_third_course/MainActivity.kt
@@ -5,11 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.wallet.AutoResolveHelper
-import com.google.android.gms.wallet.PaymentData
-import com.google.android.gms.wallet.PaymentsClient
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.wallet.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.buy_with_googlepay_button.*
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var paymentsClient: PaymentsClient
@@ -22,8 +23,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        paymentsClient = GooglePayHelper.createPaymentsClient(this)
-        GooglePayHelper.isReadyToPay(paymentsClient).addOnSuccessListener {
+        paymentsClient = GooglePayHelper.createPaymentsClient(this)!!
+
+        val isReadyToPayJson: JSONObject? = GooglePayHelper.getIsReadyToPayRequest()
+        if (isReadyToPayJson == null) {
+            return
+        }
+        val request = IsReadyToPayRequest.fromJson(isReadyToPayJson.toString()) ?: return
+
+        val task: Task<Boolean> = paymentsClient.isReadyToPay(request)
+        task.addOnSuccessListener {
             buy_with_google_button.isClickable = true
             buy_with_google_button.setOnClickListener { payWithGooglePay() }
         }
@@ -49,13 +58,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun payWithGooglePay() {
-        val request = GooglePayHelper.createPaymentDataRequest("0,0")
-        paymentsClient.let {
-            showLoading()
+        showLoading()
+
+        val paymentDataRequestJson: JSONObject? = GooglePayHelper.getPaymentDataRequest("0.0")
+        if (paymentDataRequestJson == null) {
+            return
+        }
+        val request = PaymentDataRequest.fromJson(paymentDataRequestJson.toString())
+
+        if (request != null) {
             AutoResolveHelper.resolveTask(
-                it.loadPaymentData(request),
-                this,
-                LOAD_PAYMENT_DATA_REQUEST_CODE
+                paymentsClient.loadPaymentData(request), this, LOAD_PAYMENT_DATA_REQUEST_CODE
             )
         }
     }
@@ -70,5 +83,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun showResult(result: String) {
         tv_main_result.text = result
+        tv_main_result.visibility = View.VISIBLE
     }
 }
